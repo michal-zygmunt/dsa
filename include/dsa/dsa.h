bf76bdda18837d81942d35d597887f1272e3c773
Michal Zygmunt <m_zygmunt@outlook.com>
2025-10-15 23:35:04 +0200

    Add dsa.h file with includes to implemented classes

---
+++ include/dsa/dsa.h
@@ -0,0 +1,20 @@
+/**
+ * @file dsa.h
+ * @brief This file contains includes for implemented classes
+ * @author Michal Zygmunt
+ *
+ * @copyright Copyright (c) 2025 Michal Zygmunt
+ * This project is distributed under the MIT License.
+ * See accompanying LICENSE.txt file or obtain copy at
+ * https://opensource.org/license/mit
+ */
+
+#ifndef DSA_H
+#define DSA_H
+
+#include "forward_list.h"
+#include "list.h"
+#include "queue.h"
+#include "stack.h"
+
+#endif // DSA_H
