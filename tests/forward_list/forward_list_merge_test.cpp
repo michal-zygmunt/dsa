e962e2cde4bfde38d3b52fa776ad1264b6c5a021
Michal Zygmunt <m_zygmunt@outlook.com>
2026-03-14 23:22:04 +0100

    test(ForwardList): add test for merge function using comparison function object

--- tests/forward_list/forward_list_merge_test.cpp
+++ tests/forward_list/forward_list_merge_test.cpp
@@ -14,6 +14,7 @@
 
 #include <exception>
 #include <forward_list>
+#include <functional>
 #include <initializer_list>
 #include <iostream>
 #include <utility>
@@ -81,14 +82,77 @@ int main() // NOLINT(modernize-use-trailing-return-type)
         // NOLINTNEXTLINE(bugprone-use-after-move)
         tests::compare("ForwardList12", list12, expected12);
 
-        // self merge
-        dsa::ForwardList<int> list13{ il_1 };
-        list13.merge(list13);
-        tests::compare("ForwardList13", list13, il_1);
+        // merging using non default compration argument
 
-        dsa::ForwardList<int> list14;
-        list14.merge(list14);
-        tests::compare("ForwardList14", list14, {});
+        dsa::ForwardList<int> list13 = dsa::ForwardList<int>(il_1);
+        dsa::ForwardList<int> list14 = dsa::ForwardList<int>(il_2);
+        list13.sort(std::greater<>());
+        list14.sort(std::greater<>());
+        list13.merge(list14, std::greater<>());
+        const std::initializer_list<int> expected13 = { 50, 40, 30, 20, 10, 5, 4, 3, 2, 1 };
+        tests::compare("ForwardList13", list13, expected13);
+        const std::initializer_list<int> expected14 = {};
+        tests::compare("ForwardList14", list14, expected14);
+
+        dsa::ForwardList<int> list15 = dsa::ForwardList<int>(il_1);
+        dsa::ForwardList<int> list16;
+        list15.sort(std::greater<>());
+        list16.sort(std::greater<>());
+        list16.merge(list15, std::greater<>());
+        const std::initializer_list<int> expected15{};
+        tests::compare("ForwardList15", list15, expected15);
+        const std::initializer_list<int> expected16{ 5, 4, 3, 2, 1 };
+        tests::compare("ForwardList16", list16, expected16);
+
+        dsa::ForwardList<int> list17 = dsa::ForwardList<int>(il_3);
+        dsa::ForwardList<int> list18 = dsa::ForwardList<int>(il_4);
+        list17.sort(std::greater<>());
+        list18.sort(std::greater<>());
+        list17.merge(list18, std::greater<>());
+        const std::initializer_list<int> expected17{ 9, 7, 6, 5, 4, 3, 2, 1, 1, 1 };
+        tests::compare("ForwardList17", list17, expected17);
+        const std::initializer_list<int> expected18{};
+        tests::compare("ForwardList18", list18, expected18);
+
+        dsa::ForwardList<int> list19 = dsa::ForwardList<int>(il_4);
+        dsa::ForwardList<int> list20 = dsa::ForwardList<int>(il_3);
+        list19.sort(std::greater<>());
+        list20.sort(std::greater<>());
+        list19.merge(list20, std::greater<>());
+        const std::initializer_list<int> expected19{ 9, 7, 6, 5, 4, 3, 2, 1, 1, 1 };
+        tests::compare("ForwardList19", list19, expected19);
+        const std::initializer_list<int> expected20{};
+        tests::compare("ForwardList20", list20, expected20);
+
+        dsa::ForwardList<int> list21 = dsa::ForwardList<int>(il_2);
+        dsa::ForwardList<int> list22 = dsa::ForwardList<int>(il_3);
+        list21.sort(std::greater<>());
+        list22.sort(std::greater<>());
+        list21.merge(list22, std::greater<>());
+        const std::initializer_list<int> expected21{ 50, 40, 30, 20, 10, 7, 5, 3, 1, 1 };
+        tests::compare("ForwardList21", list21, expected21);
+        const std::initializer_list<int> expected22{};
+        tests::compare("ForwardList22", list22, expected22);
+
+        dsa::ForwardList<int> list23 = dsa::ForwardList<int>(il_3);
+        dsa::ForwardList<int> list24 = dsa::ForwardList<int>(il_2);
+        list23.sort(std::greater<>());
+        list24.sort(std::greater<>());
+        list23.merge(std::move(list24), std::greater<>());
+        const std::initializer_list<int> expected23{ 50, 40, 30, 20, 10, 7, 5, 3, 1, 1 };
+        tests::compare("ForwardList23", list23, expected23);
+        const std::initializer_list<int> expected24{};
+        // NOLINTNEXTLINE(bugprone-use-after-move)
+        tests::compare("ForwardList24", list24, expected24);
+
+        // self merge
+        dsa::ForwardList<int> list25{ il_1 };
+        list25.merge(list25);
+        tests::compare("ForwardList25", list25, il_1);
+
+        dsa::ForwardList<int> list26;
+        list26.merge(list26);
+        tests::compare("ForwardList26", list26, {});
 
 
         std::cout << "Compare operations results with std container\n\n";
@@ -116,6 +180,79 @@ int main() // NOLINT(modernize-use-trailing-return-type)
         std_list7.merge(std_list8);
         tests::compare("ForwardList7 vs std", list7, std_list7);
         tests::compare("ForwardList8 vs std", list8, std_list8);
+
+        std::forward_list<int> std_list9{ il_2 };
+        std::forward_list<int> std_list10{ il_3 };
+        std_list9.merge(std_list10);
+        tests::compare("ForwardList9 vs std", list9, std_list9);
+        tests::compare("ForwardList10 vs std", list10, std_list10);
+
+        std::forward_list<int> std_list11{ il_3 };
+        std::forward_list<int> std_list12{ il_2 };
+        std_list11.merge(std::move(std_list12));
+        tests::compare("ForwardList11 vs std", list11, std_list11);
+        // NOLINTNEXTLINE(bugprone-use-after-move)
+        tests::compare("ForwardList12 vs std", list12, std_list12);
+
+        // merging using non default compration argument
+
+        std::forward_list<int> std_list13{ il_1 };
+        std::forward_list<int> std_list14{ il_2 };
+        std_list13.sort(std::greater<>());
+        std_list14.sort(std::greater<>());
+        std_list13.merge(std_list14, std::greater<>());
+        tests::compare("ForwardList13 vs std", list13, std_list13);
+        tests::compare("ForwardList14 vs std", list14, std_list14);
+
+        std::forward_list<int> std_list15{ il_1 };
+        std::forward_list<int> std_list16;
+        std_list15.sort(std::greater<>());
+        std_list16.sort(std::greater<>());
+        std_list16.merge(std_list15, std::greater<>());
+        tests::compare("ForwardList15 vs std", list15, std_list15);
+        tests::compare("ForwardList16 vs std", list16, std_list16);
+
+        std::forward_list<int> std_list17{ il_3 };
+        std::forward_list<int> std_list18{ il_4 };
+        std_list17.sort(std::greater<>());
+        std_list18.sort(std::greater<>());
+        std_list17.merge(std_list18, std::greater<>());
+        tests::compare("ForwardList17 vs std", list17, std_list17);
+        tests::compare("ForwardList18 vs std", list18, std_list18);
+
+        std::forward_list<int> std_list19{ il_4 };
+        std::forward_list<int> std_list20{ il_3 };
+        std_list19.sort(std::greater<>());
+        std_list20.sort(std::greater<>());
+        std_list19.merge(std_list20, std::greater<>());
+        tests::compare("ForwardList19 vs std", list19, std_list19);
+        tests::compare("ForwardList20 vs std", list20, std_list20);
+
+        std::forward_list<int> std_list21{ il_2 };
+        std::forward_list<int> std_list22{ il_3 };
+        std_list21.sort(std::greater<>());
+        std_list22.sort(std::greater<>());
+        std_list21.merge(std_list22, std::greater<>());
+        tests::compare("ForwardList21 vs std", list21, std_list21);
+        tests::compare("ForwardList22 vs std", list22, std_list22);
+
+        std::forward_list<int> std_list23{ il_3 };
+        std::forward_list<int> std_list24{ il_2 };
+        std_list23.sort(std::greater<>());
+        std_list24.sort(std::greater<>());
+        std_list23.merge(std::move(std_list24), std::greater<>());
+        tests::compare("ForwardList23 vs std", list23, std_list23);
+        // NOLINTNEXTLINE(bugprone-use-after-move)
+        tests::compare("ForwardList24 vs std", list24, std_list24);
+
+        // self merge
+        std::forward_list<int> std_list25{ il_1 };
+        std_list25.merge(std_list25);
+        tests::compare("ForwardList25 vs std", list25, std_list25);
+
+        std::forward_list<int> std_list26;
+        std_list26.merge(std_list26);
+        tests::compare("ForwardList26 vs std", list26, std_list26);
 
 
         tests::print_stats();
