20d2773755db039dde63e33a8c2356c8d64b9448
Michal Zygmunt <m_zygmunt@outlook.com>
2026-07-30 23:29:52 +0200

    fix(ForwardList): declare variable as const [misc-const-correctness]

--- include/dsa/forward_list.h
+++ include/dsa/forward_list.h
@@ -2178,8 +2178,8 @@ namespace dsa
         }
 
         NodeBase* result{};
-        Node* node_left = dynamic_cast<Node*>(left);
-        Node* node_right = dynamic_cast<Node*>(right);
+        const Node* node_left = dynamic_cast<Node*>(left);
+        const Node* node_right = dynamic_cast<Node*>(right);
         if (node_left && node_right)
         {
             // Recursively merge nodes
